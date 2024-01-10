04a0bc95079186676674f24043c2d72e5db16995
opilane <opilane@HP-CZC2349HT0>
2024-01-10 14:57:35 +0200

    vigade otsind 2.0

--- PythonKar2.py
+++ PythonKar2.py
@@ -1,36 +1,36 @@
 
 #vigade otsing/moodle 
 
-import math
+import math 
 
 
 print("Ruudu karakteristikud")
 
 a=float(input('Sisesta ruudu külje pikkus => '))
 S=a**2
-print("Ruudu pindala:",S)
+print("Ruudu pindala",round(S,1))
 P=4*a
-print("Ruudu ümbermõõt:",P)
+print("Ruudu ümbermõõt",round(P,1))
 di=a*math.sqrt(2)
-print("Ruudu diagonaal:", round(di,2))
+print("Ruudu diagonaal", round(di,2))
 print()
 
 print("Ristküliku karakteristikud")
 b=float(input("Sisesta ristküliku 1. külje pikkus => "))
 c=float(input("Sisesta ristküliku 2. külje pikkus => "))
-S=b * c
+S=b*c
 
-print("Ristküliku pindala:",S)
+print("Ristküliku pindala:",round(S,1))
 P=2* (b+c)
-print("Ristküliku ümbermõõt:",P)
+print("Ristküliku ümbermõõt",round(P,1))
 di = math.sqrt(b**2 + c**2)
-print("Ristküliku diagonaal:", round(di, 2))
+print("Ristküliku diagonaal:", round(di,1))
 print()
 
 print("Ringi karakteristikud")
 r=float(input("Sisesta ringi raadiuse pikkus => "))
 d=2*r
-print("Ringi läbimõõt:",d)
+print("Ringi läbimõõt:",round(d,1))
 S=math.pi * r**2
 print("Ringi pindala:", round(S,2))
 C=2 * math.pi * r
